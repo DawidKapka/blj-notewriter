ed34501f3e43abd8f0a49ee2aff5ecc188031d4c
dawidkapka <kapkadawid6@gmail.com>
2020-12-04 09:52:58 +0100

    basic text detection

---
+++ detection.py
@@ -0,0 +1,11 @@
+from craft_text_detector import Craft
+
+image_path = 'img/download.png'
+output_dir = 'outputs/'
+
+craft = Craft(output_dir=output_dir, crop_type="poly", cuda=False)
+
+prediction_result = craft.detect_text(image_path)
+
+craft.unload_craftnet_model()
+craft.unload_refinenet_model()
