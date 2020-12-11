28b3c44004d17b7d8c45d298ae1feb1fdbeeef0b
dawidkapka <kapkadawid6@gmail.com>
2020-12-11 09:02:54 +0100

    changed folder structure

--- api.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_restful import Resource, Api
-import os
-import detection
-
-app = Flask(__name__)
-api = Api(app)
-
-#work in progress
--- notewriter_app/android/app/src/main/kotlin/com/example/notewriter_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.notewriter_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
